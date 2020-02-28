5816ca2f3e1c49e575b5b40e08a14674b4d49836
Chris Olesen <privat@chrisolesen.net>
2020-02-28 12:29:16 +0100

    initial commits

---
+++ app_core/cron_files/one_hour.php
@@ -0,0 +1,24 @@
+<?php
+define('cron_by', 'one_hour');
+$basepath = '/home/praktisk/';
+
+date_default_timezone_set('Europe/Copenhagen');
+$current_date = new DateTime('now');
+$date_now = $current_date->format('Y-m-d');
+$time_now = $current_date->format('H:i:s');
+
+$log_content = PHP_EOL . PHP_EOL . '#######################################################'
+	. PHP_EOL . '# One hour cron started at ' . $time_now;
+file_put_contents("app_core/cron_files/logs/cron_one_hour_{$date_now}", $log_content, FILE_APPEND);
+
+include_once "{$basepath}app_core/cron_files/functions/die.php";
+include_once "{$basepath}app_core/cron_files/functions/grow_up.php";
+include_once "{$basepath}app_core/cron_files/functions/give_birth.php";
+if ($time_now > '18:00:00' && $time_now < '19:00:00') {
+	include_once "{$basepath}app_core/cron_files/functions/automate_contests.php";
+}
+
+$log_content = ''
+	. PHP_EOL . '# Cron compleated its run. '
+	. PHP_EOL . '#######################################################' . PHP_EOL;
+file_put_contents("app_core/cron_files/logs/cron_one_hour_{$date_now}", $log_content, FILE_APPEND);
--- app_core/functions/months_ago.php
+++ app_core/functions/months_ago.php
@@ -1,6 +1,7 @@
 <?php
 
 function months_ago($date) {
+	$i = 0;
 	$one_month = new DateInterval("P1M");
 	$date_now = new DateTime('now');
 	while ($date < $date_now->format('Y-m-d H:i:s')) {
