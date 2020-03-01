9e5bdec85deda1d582915e0dbf6a96fbf6b66384
Chris Olesen <privat@chrisolesen.net>
2020-03-01 20:44:33 +0100

    Update basic_data.php

--- net-hesten.dk/install/basic_data.php
+++ net-hesten.dk/install/basic_data.php
@@ -7,12 +7,17 @@ $sql = 'INSERT INTO praktisk_dev_nethest_new.horse_races';
 /*$result = $sth->fetchAll();*/
 /* Insert a horse race */
 $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_races` LIMIT 1";
-$race = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY))->fetch();
+try {
+    $race = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY))->fetch();
+} catch (Exception $e) {
+    $pdo->rollback();
+    throw $e;
+}
 var_dump($race);
 if (!$race) {
     $sql = "INSERT INTO `{$GLOBALS['DB_NAME_NEW']}`.`horse_races` (`name`,`max_height`,`min_height`,`description`) VALUES (:name, :max_height, :min_height, :description)";
-    $statement = $GLOBALS['pdo_new']->prepare($sql);
     try {
+        $statement = $GLOBALS['pdo_new']->prepare($sql);
         $GLOBALS['pdo_new']->beginTransaction();
         $statement->execute(['name' => 'Ghosts', 'max_height' => 170, 'min_height' => 150, 'description' => 'Mythical creatures']);
         $GLOBALS['pdo_new']->commit();
