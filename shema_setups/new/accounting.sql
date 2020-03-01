55694f64ba4c0031eb510df0ae7197c9254c5874
Chris Olesen <privat@chrisolesen.net>
2020-03-01 14:24:35 +0100

    shema files

---
+++ shema_setups/new/accounting.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `game_data_accounting` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `entry_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `line_meta` blob NOT NULL,
+  `line_amount` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
