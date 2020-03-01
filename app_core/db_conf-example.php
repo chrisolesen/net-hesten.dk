0c51d3b6f0842daba494b002d9b7ebd3f069e86c
Chris Olesen <privat@chrisolesen.net>
2020-03-01 14:40:28 +0100

    installer

--- app_core/db_conf-example.php
+++ app_core/db_conf-example.php
@@ -12,16 +12,20 @@ define('DB_PASS', '');
 define('DB_PORT', '3306');
 define('DB_NAME_OLD', '');
 define('DB_NAME_NEW', '');
+$mysql_host = DB_HOST;
+$mysql_user = DB_USER;
+$mysql_password = DB_PASS;
+$mysql_database_old = DB_NAME_OLD;
+$mysql_database_new = DB_NAME_NEW;
 $_GLOBALS['DB_NAME_OLD'] = DB_NAME_OLD;
 $_GLOBALS['DB_NAME_NEW'] = DB_NAME_NEW;
 
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME_OLD, DB_PORT);
-if (!$link) {
-	die('<!--Could not connect: ' . mysqli_error($link) . '-->');
-}
-$link_old = $link;
+$link_old = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME_OLD, DB_PORT);
 $link_old->set_charset('latin1');
 $link_old->query("SET time_zone = 'Europe/Copenhagen'");
 $link_new = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME_NEW, DB_PORT);
 $link_new->set_charset('utf8');
 $link_new->query("SET time_zone = 'Europe/Copenhagen'");
+
+$GLOBALS['pdo_new'] = new PDO("mysql:host=$mysql_host;dbname=$mysql_database_new", $mysql_user, $mysql_password);
+$GLOBALS['pdo_old'] = new PDO("mysql:host=$mysql_host;dbname=$mysql_database_old", $mysql_user, $mysql_password);
