0f5245238d74bb1da316b5b74982e69bcb67d0ee
Chris Olesen <privat@chrisolesen.net>
2020-03-02 14:35:39 +0100

    Update basic_data.php

--- net-hesten.dk/install/basic_data.php
+++ net-hesten.dk/install/basic_data.php
@@ -3,39 +3,65 @@ $basepath = '../..';
 require_once("{$basepath}/app_core/db_conf.php");
 require_once("{$basepath}/app_core/functions/password_hash.php");
 ?>
-<a href="/install/">go back</a>
-<?php
+<!DOCTYPE html>
+<html>
 
-/*$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` WHERE id = :user_id";*/
+<head>
+    <title>Net-hesten - Project installer</title>
+</head>
 
-if (filter_input(INPUT_POST, 'install_action') === 'install_admin_user') {
-    $sql = "INSERT INTO `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` 
-        (`stutteri`, `password`, `date`) VALUES (:user_name, :user_pass, NOW())";
-    $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-    $sth->execute(['user_name' => filter_input(INPUT_POST, 'username'), 'user_pass' => cbc_pwhash(filter_input(INPUT_POST, 'password'))]);
-}
+<body>
+    <a href="/install/">Go back</a>
+    <?php
 
-$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` LIMIT 1";
-$sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-$sth->execute();
-if (!$sth->rowCount()) {
-    /* No admin user detected */
-?>
-    <form method="post" action="">
-        <input type="hidden" name="install_action" value="install_admin_user" />
-        <input type="text" name="username" placeholder="Username:" />
-        <input type="text" name="password" placeholder="Password:" />
-        <input type="submit" name="save_admin" />
-    </form>
-<?php
-    /* while (
-        $row = $sth->fetch(PDO::FETCH_OBJ)
-    ) {
-        var_dump($row);
+    if (filter_input(INPUT_POST, 'install_action') === 'install_admin_user') {
+        $sql = "INSERT INTO `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` 
+    (`stutteri`, `password`, `date`, `penge`) VALUES (:user_name, :user_pass, NOW(), 1000000)";
+        $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute(['user_name' => filter_input(INPUT_POST, 'username'), 'user_pass' => cbc_pwhash(filter_input(INPUT_POST, 'password'))]);
     }
-    echo 'found user 1'; */
-}
 
 
 
-?>
+    $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` LIMIT 1";
+    $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute();
+    if (!$sth->rowCount()) {
+        /* No admin user detected */
+    ?>
+        <form method="post" action="">
+            <input type="hidden" name="install_action" value="install_admin_user" />
+            <input type="text" name="username" placeholder="Username:" />
+            <input type="text" name="password" placeholder="Password:" />
+            <input type="submit" name="save_admin" />
+        </form>
+    <?php
+    }
+    /* Initialize horse races */
+    $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`horse_races` LIMIT 1";
+    $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute();
+    if (!$sth->rowCount()) {
+        /* Insert horse race */
+        $sql = "INSERT INTO `{$GLOBALS['DB_NAME_OLD']}`.`horse_races` 
+    (`name`, `min_height`, `max_height`, `description`) VALUES 
+    ('Ghosts', 150, 170, 'Mythical creatures')";
+        $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute();
+    }
+    /* Initialize horse types */
+    $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`horse_types` LIMIT 1";
+    $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute();
+    if (!$sth->rowCount()) {
+        /* Insert horse type */
+        $sql = "INSERT INTO `{$GLOBALS['DB_NAME_OLD']}`.`horse_types` 
+    (`race`, `image`, `status`, `date`) VALUES 
+    ('Ghosts', 'Ghost_01.png', 22, NOW())";
+        $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute();
+    }
+    ?>
+</body>
+
+</html>
