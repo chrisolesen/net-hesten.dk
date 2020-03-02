ab45ac02629a6a5ad5c80240476d4853aeb4387f
Chris Olesen <privat@chrisolesen.net>
2020-03-02 14:14:25 +0100

    password hash

--- net-hesten.dk/install/basic_data.php
+++ net-hesten.dk/install/basic_data.php
@@ -1,72 +1,41 @@
 <?php
 $basepath = '../..';
 require_once("{$basepath}/app_core/db_conf.php");
+require_once("{$basepath}/app_core/functions/password_hash.php");
+?>
+<a href="/install/">go back</a>
+<?php
 
-/*$sql = 'INSERT INTO praktisk_dev_nethest_new.horse_races';*/
+/*$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` WHERE id = :user_id";*/
 
-$users = $link_old->query("SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` WHERE id = 1");
-while ($row = $users->fetch_object()) {
-    var_dump($row);
-}
-echo "<br />result via mysqli<br />";
-
-
-$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` WHERE id = :user_id";
-$sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-$sth->execute([':user_id' => 150]);
-if ($sth->rowCount()) {
-    echo '<br />found user 150<br />';
-} else {
-    echo '<br />no user 150<br />';
+if (filter_input(INPUT_POST, 'install_action') === 'install_admin_user') {
+    $sql = "INSERT INTO `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` 
+        (`stutteri`, `password`, `date`) VALUES (:user_name, :user_pass, NOW())";
+    $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute(['user_name' => filter_input(INPUT_POST, 'username'), 'user_pass' => cbc_pwhash(filter_input(INPUT_POST, 'password'))]);
 }
 
-$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` WHERE id = :user_id";
+$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` LIMIT 1";
 $sth = $GLOBALS['pdo_old']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-$sth->execute([':user_id' => 1]);
-if ($sth->rowCount()) {
-    while (
+$sth->execute();
+if (!$sth->rowCount()) {
+    /* No admin user detected */
+?>
+    <form method="post" action="">
+        <input type="hidden" name="install_action" value="install_admin_user" />
+        <input type="text" name="username" placeholder="Username:" />
+        <input type="text" name="password" placeholder="Password:" />
+        <input type="submit" name="save_admin" />
+    </form>
+<?php
+    /* while (
         $row = $sth->fetch(PDO::FETCH_OBJ)
     ) {
         var_dump($row);
     }
-    echo 'found user 1';
-} else {
-    echo 'no user 1';
-}
-exit();
-
-/*$result = $sth->fetchAll();*/
-/* Insert a horse race */
-$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_races` LIMIT 1";
-try {
-    $statement = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY))->execute();
-    $race = $statement->fetch();
-} catch (Exception $e) {
-    /* $GLOBALS['pdo_new']->rollback();*/
-    throw $e;
-}
-var_dump($race);
-if (!$race) {
-    $sql = "INSERT INTO `{$GLOBALS['DB_NAME_NEW']}`.`horse_races` (`name`,`max_height`,`min_height`,`description`) VALUES (:name, :max_height, :min_height, :description)";
-    try {
-        $statement = $GLOBALS['pdo_new']->prepare($sql);
-        $GLOBALS['pdo_new']->beginTransaction();
-        $statement->execute(['name' => 'Ghosts', 'max_height' => 170, 'min_height' => 150, 'description' => 'Mythical creatures']);
-        $GLOBALS['pdo_new']->commit();
-    } catch (Exception $e) {
-        $GLOBALS['pdo_new']->rollback();
-        throw $e;
-    }
+    echo 'found user 1'; */
 }
 
-$sql = "SELECT id FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` LIMIT 1";
-$user = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY))->fetch();
-var_dump($user);
-/*
-INSERT into `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` (stutteri, password, navn, email, hestetegner, beskrivelse, penge, date) VALUES ('admin_usr', 'pass', 'name', 'mail', 'ja', '', '100000000', NOW());
-*//*
-$stmt = $pdo->query("SELECT * FROM users ORDER BY id DESC LIMIT 1");
-$user = $stmt->fetch();
-*/
+
+
 ?>
-<a href="/install/">go back</a>
