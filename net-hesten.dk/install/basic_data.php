2c1b07736179674d51bb347a8c39a74c9052891e
Chris Olesen <privat@chrisolesen.net>
2020-03-07 16:33:42 +0100

    ghost horses

--- net-hesten.dk/install/basic_data.php
+++ net-hesten.dk/install/basic_data.php
@@ -73,7 +73,7 @@ require_once("{$basepath}/app_core/functions/password_hash.php");
         $sth->execute();
     }
     /* Initialize horse types */
-    $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` LIMIT 1";
+    $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` WHERE `image` = 'ghost_horse.png' LIMIT 1";
     $sth = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
     $sth->execute();
     if (!$sth->rowCount()) {
@@ -81,6 +81,18 @@ require_once("{$basepath}/app_core/functions/password_hash.php");
         $sql = "INSERT INTO `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` 
     (`race`, `image`, `status`, `date`, `allowed_gender`, `archived`, `artists`) VALUES 
     ('Ghosts', 'Ghost_01.png', 22, NOW(), 1,0,0)";
+        $sth = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sth->execute();
+    }
+
+    $sql = "SELECT id FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` WHERE `image` = 'ghost_foel.png' LIMIT 1";
+    $sth = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sth->execute();
+    if (!$sth->rowCount()) {
+        /* Insert horse type */
+        $sql = "INSERT INTO `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` 
+    (`race`, `image`, `status`, `date`, `allowed_gender`, `archived`, `artists`) VALUES 
+    ('Ghosts', 'ghost_foel.png', 22, NOW(), 1,0,0)";
         $sth = $GLOBALS['pdo_new']->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
         $sth->execute();
     }
