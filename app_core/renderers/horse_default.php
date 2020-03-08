8aea352da330c55b2a9d7ca999aa3085a296e50b
Chris Olesen <privat@chrisolesen.net>
2020-03-08 09:16:58 +0100

    Update horse_default.php

--- app_core/renderers/horse_default.php
+++ app_core/renderers/horse_default.php
@@ -17,7 +17,7 @@ function render_horse_object($horse, $area)
     <div class='horse_square horse_object <?= $gender; ?>' data-horse-id='<?= $horse['id']; ?>'>
         <div class='info'>
             <span class='name'><?= $horse['name']; ?></span>
-            <i class='gender icon-{$gender}-1'></i>
+            <i class='gender icon-<?= $gender; ?>-1'></i>
             <div class='horse_vcard'>
                 <i class='icon-vcard'></i>
                 <div class='extended_info'>
@@ -29,9 +29,47 @@ function render_horse_object($horse, $area)
                     <span class='disability'>Ulempe: <?= $horse['ulempe']; ?></span><br />
                     <span class='talent'>Talent: <?= $horse['talent']; ?></span><br /><br />
                     <span class='artist'>Tegner: <?= $horse['artist']; ?></span>
-                    <span class='value'>Værdi: <?= number_dotter($horse['value']); ?><span class="wkr_symbol">wkr</span></span>
+                    <?php if (in_array($area, ['main_stud'])) { ?>
+                        <span class='value'>Værdi: <?= number_dotter($horse['value']); ?><span class="wkr_symbol">wkr</span></span>
+                    <?php } elseif (in_array($area, ['horse_trader_sell'])) { ?>
+                        <span class='value' style="transform: translateY(-18px);">Værdi: <?= number_dotter($horse->value); ?> <span class="wkr_symbol">wkr</span></span>
+                        <span class='value'>Sælg for: <?= number_dotter($horse->value * 0.9); ?> <span class="wkr_symbol">wkr</span></span>
+                    <?php } ?>
                 </div>
             </div>
+            <?php if (in_array($area, ['horse_trader_sell'])) { ?>
+                <?php if ($_SESSION['settings']['horse_trader_buy_confirmations'] == 'show') { ?>
+                    <button class='open_sell_window btn btn-success compact_bottom_button'>Sælg</button>
+                    <form action="/area/world/horsetrader/sell.php" method="post" class="action_popup buy_horse">
+                        <?php /* code to place bid */ ?>
+                        <input type="hidden" name="action" value="sell_horse_to_trader" />
+                        <input type="hidden" name="horse_id" value="<?= $horse->id; ?>" />
+                        <?php if (filter_input(INPUT_POST, 'race') && !empty(filter_input(INPUT_POST, 'race'))) { ?>
+                            <input type="hidden" name="race" value="<?= filter_input(INPUT_POST, 'race'); ?>" />
+                        <?php } ?>
+                        <?php if (filter_input(INPUT_POST, 'gender') && !empty(filter_input(INPUT_POST, 'gender'))) { ?>
+                            <input type="hidden" name="gender" value="<?= filter_input(INPUT_POST, 'gender'); ?>" />
+                        <?php } ?>
+                        <div>ID: <?= $horse->id; ?><br /><br /></div>
+                        <div>Du for: <?= number_dotter(($horse->value * 0.9)); ?> <span class="wkr_symbol">wkr</span><br /><br /></div>
+                        <input type='submit' class='btn btn-success' name='sell_now' value="Sælg nu" />
+                        <button class='close_sell_window btn btn-danger'>Luk</button>
+                    </form>
+                <?php } else { ?>
+                    <form action="/area/world/horsetrader/sell.php" method="post" class="nonconfirmation_form visible open_sell_window btn btn-success compact_bottom_button">
+                        <?php if (filter_input(INPUT_POST, 'race') && !empty(filter_input(INPUT_POST, 'race'))) { ?>
+                            <input type="hidden" name="race" value="<?= filter_input(INPUT_POST, 'race'); ?>" />
+                        <?php } ?>
+                        <?php if (filter_input(INPUT_POST, 'gender') && !empty(filter_input(INPUT_POST, 'gender'))) { ?>
+                            <input type="hidden" name="gender" value="<?= filter_input(INPUT_POST, 'gender'); ?>" />
+                        <?php } ?>
+                        <input type="hidden" name="action" value="sell_horse_to_trader" />
+                        <input type="hidden" name="horse_id" value="<?= $horse->id; ?>" />
+                        <input type='submit' class='btn btn-success' name='sell_now' value="Sælg nu" />
+                    </form>
+                <?php } ?>
+            <?php } ?>
+
             <?php
             $disallow_breeding = false;
             if ($horse['status'] == 'føl' || $horse['staevne'] == 'ja' || $horse['graesning'] == 'ja' || $gender == 'male' || $horse_is_at_competition || $horse['breed_date']) {
