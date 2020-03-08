966d07d5eba45a90991414cebc51ac9a0790725c
Chris Olesen <privat@chrisolesen.net>
2020-03-08 09:10:24 +0100

    link

--- global_modules/modals/horze_extended_info.php
+++ global_modules/modals/horze_extended_info.php
@@ -1,66 +1,65 @@
 <div id="horze_extended_info" class="modal"><?php /* */ ?>
     <script>
-
-		function horze_extended_info(caller) {
-			jQuery.getJSON({
-				dataType: 'jsonp',
-				data: {
-					'request': 'fecth_extended_info',
-					'horse_id': jQuery(caller).parent().attr('data-horse-id')
-				},
-				crossDomain: true,
-				url: "https://ajax.net-hesten.dk/index.php",
-				cache: false
-			}).always(function (data) {
-				horse_data = data.horse_data;
-				jQuery('#horze_extended_info .name').html(horse_data.name);
-				jQuery('#horze_extended_info .id').html(horse_data.id);
-				jQuery('#horze_extended_info .age').html(horse_data.age);
-				jQuery('#horze_extended_info .gender').html(horse_data.gender);
-				jQuery('#horze_extended_info .race').html(horse_data.race);
-				jQuery('#horze_extended_info .artist').html(horse_data.artist);
-				jQuery('#horze_extended_info .value').html(horse_data.value);
-				jQuery('#horze_extended_info .owner_name').html(horse_data.owner_name);
-				jQuery('#horze_extended_info .talent').html(horse_data.talent);
-				jQuery('#horze_extended_info .ulempe').html(horse_data.ulempe);
-				jQuery('#horze_extended_info .egenskab').html(horse_data.egenskab);
-				console.log(horse_data.type);
-				if (horse_data.type == null) {
-					jQuery('#horze_extended_info .type').parent().hide();
-				} else {
-					jQuery('#horze_extended_info .type').parent().show();
-					jQuery('#horze_extended_info .type').html(horse_data.type);
-				}
-				if (horse_data.gold_medal == 0) {
-					jQuery('#horze_extended_info .gold_medal').parent().hide();
-				} else {
-					jQuery('#horze_extended_info .gold_medal').parent().show();
-					jQuery('#horze_extended_info .gold_medal').html(horse_data.gold_medal);
-				}
-				if (horse_data.silver_medal == 0) {
-					jQuery('#horze_extended_info .silver_medal').parent().hide();
-				} else {
-					jQuery('#horze_extended_info .silver_medal').parent().show();
-					jQuery('#horze_extended_info .silver_medal').html(horse_data.silver_medal);
-				}
-				if (horse_data.bronze_medal == 0) {
-					jQuery('#horze_extended_info .bronze_medal').parent().hide();
-				} else {
-					jQuery('#horze_extended_info .bronze_medal').parent().show();
-					jQuery('#horze_extended_info .bronze_medal').html(horse_data.bronze_medal);
-				}
-				if (horse_data.junior_medal == 0) {
-					jQuery('#horze_extended_info .junior_medal').parent().hide();
-				} else {
-					jQuery('#horze_extended_info .junior_medal').parent().show();
-					jQuery('#horze_extended_info .junior_medal').html(horse_data.junior_medal);
-				}
-				jQuery('#horze_extended_info [data-target="horse_linage"]').attr('data-horse-id', horse_data.id);
-				if (data.status !== true) {
-					console.log('Der skete en fejl ved omdøbning, vent lidt tid, hvis fejlen bliver ved, så skriv hestens ID til Tækhesten');
-				}
-			});
-		}
+        function horze_extended_info(caller) {
+            jQuery.getJSON({
+                dataType: 'jsonp',
+                data: {
+                    'request': 'fecth_extended_info',
+                    'horse_id': jQuery(caller).parent().attr('data-horse-id')
+                },
+                crossDomain: true,
+                url: "https://ajax.<?= HTTP_HOST; ?>/index.php",
+                cache: false
+            }).always(function(data) {
+                horse_data = data.horse_data;
+                jQuery('#horze_extended_info .name').html(horse_data.name);
+                jQuery('#horze_extended_info .id').html(horse_data.id);
+                jQuery('#horze_extended_info .age').html(horse_data.age);
+                jQuery('#horze_extended_info .gender').html(horse_data.gender);
+                jQuery('#horze_extended_info .race').html(horse_data.race);
+                jQuery('#horze_extended_info .artist').html(horse_data.artist);
+                jQuery('#horze_extended_info .value').html(horse_data.value);
+                jQuery('#horze_extended_info .owner_name').html(horse_data.owner_name);
+                jQuery('#horze_extended_info .talent').html(horse_data.talent);
+                jQuery('#horze_extended_info .ulempe').html(horse_data.ulempe);
+                jQuery('#horze_extended_info .egenskab').html(horse_data.egenskab);
+                console.log(horse_data.type);
+                if (horse_data.type == null) {
+                    jQuery('#horze_extended_info .type').parent().hide();
+                } else {
+                    jQuery('#horze_extended_info .type').parent().show();
+                    jQuery('#horze_extended_info .type').html(horse_data.type);
+                }
+                if (horse_data.gold_medal == 0) {
+                    jQuery('#horze_extended_info .gold_medal').parent().hide();
+                } else {
+                    jQuery('#horze_extended_info .gold_medal').parent().show();
+                    jQuery('#horze_extended_info .gold_medal').html(horse_data.gold_medal);
+                }
+                if (horse_data.silver_medal == 0) {
+                    jQuery('#horze_extended_info .silver_medal').parent().hide();
+                } else {
+                    jQuery('#horze_extended_info .silver_medal').parent().show();
+                    jQuery('#horze_extended_info .silver_medal').html(horse_data.silver_medal);
+                }
+                if (horse_data.bronze_medal == 0) {
+                    jQuery('#horze_extended_info .bronze_medal').parent().hide();
+                } else {
+                    jQuery('#horze_extended_info .bronze_medal').parent().show();
+                    jQuery('#horze_extended_info .bronze_medal').html(horse_data.bronze_medal);
+                }
+                if (horse_data.junior_medal == 0) {
+                    jQuery('#horze_extended_info .junior_medal').parent().hide();
+                } else {
+                    jQuery('#horze_extended_info .junior_medal').parent().show();
+                    jQuery('#horze_extended_info .junior_medal').html(horse_data.junior_medal);
+                }
+                jQuery('#horze_extended_info [data-target="horse_linage"]').attr('data-horse-id', horse_data.id);
+                if (data.status !== true) {
+                    console.log('Der skete en fejl ved omdøbning, vent lidt tid, hvis fejlen bliver ved, så skriv hestens ID til Tækhesten');
+                }
+            });
+        }
     </script>
     <style>
         #horze_extended_info div {
