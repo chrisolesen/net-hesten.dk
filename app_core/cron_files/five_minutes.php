c840330d12d96431ab2a85114c5147b0c44aea2b
Chris Olesen <privat@chrisolesen.net>
2020-04-25 17:37:41 +0200

    Update five_minutes.php

--- app_core/cron_files/five_minutes.php
+++ app_core/cron_files/five_minutes.php
@@ -13,8 +13,8 @@ $cron_interval = 'five_minutes';
 include_once "{$basepath}app_core/cron_files/functions/get_older.php";
 
 $log_content = PHP_EOL . PHP_EOL . '#######################################################'
-		. PHP_EOL . '# Five minutes cron started at ' . $time_now
-		. PHP_EOL . '# Checking for ended auctions. ';
+	. PHP_EOL . '# Five minutes cron started at ' . $time_now
+	. PHP_EOL . '# Checking for ended auctions. ';
 file_put_contents("{$basepath}/app_core/cron_files/logs/cron_five_minutes_{$date_now}", $log_content, FILE_APPEND);
 
 require_once "{$basepath}app_core/db_conf.php";
@@ -23,15 +23,15 @@ require_once "{$basepath}app_core/object_handlers/accounting.php";
 /* Find no bid auctions */
 $log_content = '';
 $sql = ""
-		. "SELECT "
-		. "auctions.*, "
-		. "bids.auction "
-		. "FROM game_data_auctions AS auctions "
-		. "LEFT JOIN game_data_auction_bids AS bids "
-		. "ON bids.auction = auctions.id "
-		. "WHERE end_date < '{$date_now} {$time_now}' "
-		. "AND ISNULL(bids.auction) "
-		. "AND auctions.status_code = 1 ";
+	. "SELECT "
+	. "auctions.*, "
+	. "bids.auction "
+	. "FROM game_data_auctions AS auctions "
+	. "LEFT JOIN game_data_auction_bids AS bids "
+	. "ON bids.auction = auctions.id "
+	. "WHERE end_date < '{$date_now} {$time_now}' "
+	. "AND ISNULL(bids.auction) "
+	. "AND auctions.status_code = 1 ";
 $result = $link_new->query($sql);
 
 $num_horses = 0;
@@ -52,24 +52,24 @@ $log_content .= PHP_EOL . "# {$num_auctions} Auctions had no bids.";
 $log_content .= PHP_EOL . "# {$num_horses} Horses returned to owners.";
 file_put_contents("{$basepath}/app_core/cron_files/logs/cron_five_minutes_{$date_now}", $log_content, FILE_APPEND);
 
-$five_in_past = $current_date->sub(new DateInterval('PT5M'));
+$five_in_past = $current_date->sub(new DateInterval('PT5M'))->format('Y-m-d H:i:s');
 
 $log_content = PHP_EOL . "# Looking for winning bids.";
 $sql = ''
-		. 'SELECT '
-		. 'auctions.id AS auction_id, '
-		. 'auctions.creator AS seller, '
-		. 'auctions.object_id AS object_id, '
-		. 'bids.creator AS winner, '
-		. 'bids.bid_amount AS winning_amount, '
-		. 'bids.bid_date AS bid_date '
-		. 'FROM game_data_auction_bids AS bids '
-		. 'LEFT JOIN game_data_auctions AS auctions '
-		. 'ON bids.auction = auctions.id '
-		. "WHERE end_date < '{$five_in_past->format('Y-m-d H:i:s')}' "
-		. 'AND bids.status_code = 4 '
-		. 'AND auctions.status_code = 1 '
-		. 'ORDER BY bids.auction ASC, bids.bid_amount DESC';
+	. 'SELECT '
+	. 'auctions.id AS auction_id, '
+	. 'auctions.creator AS seller, '
+	. 'auctions.object_id AS object_id, '
+	. 'bids.creator AS winner, '
+	. 'bids.bid_amount AS winning_amount, '
+	. 'bids.bid_date AS bid_date '
+	. 'FROM game_data_auction_bids AS bids '
+	. 'LEFT JOIN game_data_auctions AS auctions '
+	. 'ON bids.auction = auctions.id '
+	. "WHERE end_date < '{$five_in_past}' "
+	. 'AND bids.status_code = 4 '
+	. 'AND auctions.status_code = 1 '
+	. 'ORDER BY bids.auction ASC, bids.bid_amount DESC';
 $result = $link_new->query($sql);
 $num_auctions = 0;
 if ($result) {
@@ -101,29 +101,29 @@ if ($result) {
 		$log_content .= PHP_EOL . "# Moving horse with ID {$data['object_id']} to user {$user_id_name_array[$data['winner']][0]} with ID {$data['winner']}.";
 		$sql = "UPDATE `{$_GLOBALS['DB_NAME_OLD']}`.Heste SET bruger = '{$user_id_name_array[$data['winner']][0]}' WHERE id = {$data['object_id']} AND bruger = 'Auktionshuset'";
 		$link_new->query($sql);
-		
+
 		$auction_fee = round(max(500, ($data['winning_amount'] * 0.005)), 0);
 		$earnings = $data['winning_amount'] - $auction_fee;
 		accounting::add_entry(['amount' => $earnings, 'line_text' => "Solgt en hest på auktion", 'mode' => '+', 'user_id' => $data['seller']]);
-		
-		
-//		$sql = "UPDATE Brugere SET penge = (penge + {$earnings}) WHERE id = {$data['seller']}";
-//		$link_new->query($sql);
+
+
+		//		$sql = "UPDATE Brugere SET penge = (penge + {$earnings}) WHERE id = {$data['seller']}";
+		//		$link_new->query($sql);
 		$sql = "UPDATE game_data_auctions SET status_code = 2 WHERE id = {$data['auction_id']}";
 		$link_new->query($sql);
 		$sql = "UPDATE game_data_auction_bids SET status_code = 6 WHERE auction = {$data['auction_id']} AND bid_date = '{$data['bid_date']}' AND creator = {$data['winner']}";
 		$link_new->query($sql);
 		/* Send besked til den ny ejer */
 		$sql = "INSERT INTO game_data_private_messages "
-				. "(status_code, origin, target, date, message) "
-				. "VALUES "
-				. "(17, 52745, {$data['winner']}, NOW(), 'Tillykke {$user_id_name_array[$data['winner']][0]}, du har vundet en auktion, med et bud på {$data['winning_amount']} wkr. Hesten med ID: {$data['object_id']}, er nu din.')";
+			. "(status_code, origin, target, date, message) "
+			. "VALUES "
+			. "(17, 52745, {$data['winner']}, NOW(), 'Tillykke {$user_id_name_array[$data['winner']][0]}, du har vundet en auktion, med et bud på {$data['winning_amount']} wkr. Hesten med ID: {$data['object_id']}, er nu din.')";
 		$result = $link_new->query($sql);
 		/* Send besked til den gamle ejer */
 		$sql = "INSERT INTO game_data_private_messages "
-				. "(status_code, origin, target, date, message) "
-				. "VALUES "
-				. "(17, 52745, {$data['seller']}, NOW(), 'Tillykke {$user_id_name_array[$data['seller']][0]}, det er lykkeds at sælge din hest på auktion, for {$data['winning_amount']} wkr!')";
+			. "(status_code, origin, target, date, message) "
+			. "VALUES "
+			. "(17, 52745, {$data['seller']}, NOW(), 'Tillykke {$user_id_name_array[$data['seller']][0]}, det er lykkeds at sælge din hest på auktion, for {$data['winning_amount']} wkr!')";
 		$result = $link_new->query($sql);
 		/* Opret indlæg i den gamle ejers 'konto' */
 
@@ -136,6 +136,6 @@ file_put_contents("{$basepath}/app_core/cron_files/logs/cron_five_minutes_{$date
 
 
 $log_content = ''
-		. PHP_EOL . '# Cron compleated its run. '
-		. PHP_EOL . '#######################################################' . PHP_EOL;
+	. PHP_EOL . '# Cron compleated its run. '
+	. PHP_EOL . '#######################################################' . PHP_EOL;
 file_put_contents("{$basepath}/app_core/cron_files/logs/cron_five_minutes_{$date_now}", $log_content, FILE_APPEND);
