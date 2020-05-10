46d92bb7de046cdb8e2f8cc49fd98718ce30dd62
Chris Olesen <privat@chrisolesen.net>
2020-05-10 20:48:41 +0200

    Update auctions.php

--- app_core/object_handlers/auctions.php
+++ app_core/object_handlers/auctions.php
@@ -15,19 +15,13 @@ class auctions
 		foreach ($attr as $key => $value) {
 			$attr[$key] = $link_new->real_escape_string($value);
 		}
-		$sql =  'SELECT '
-			. 'a.object_id AS horse_id, '
-			. 'a.status_code AS auction_status, '
-			. 'b.bid_amount, '
-			. 'b.bid_date, '
-			. 'b.status_code AS bid_status, '
-			. 'b.auction, '
-			. 'b.creator '
-			. 'FROM game_data_auction_bids AS b '
-			. 'LEFT JOIN game_data_auctions AS a '
-			. 'ON a.id = b.auction '
-			. "WHERE b.creator = {$_SESSION['user_id']} "
-			. "ORDER BY b.auction DESC, b.bid_amount DESC";
+		$sql =  "SELECT `a`.`object_id` AS `horse_id`, `a`.`status_code` AS `auction_status`, `b`.`bid_amount`, `b`.`bid_date`,
+		 `b`.`status_code` AS `bid_status`, `b`.`auction`, `b`.`creator` 
+		FROM `game_data_auction_bids` AS `b` 
+		LEFT JOIN `game_data_auctions` AS `a` 
+		ON `a`.`id` = `b`.`auction` 
+		WHERE `b`.`creator` = {$_SESSION['user_id']} 
+		ORDER BY `b`.`auction` DESC, `b`.`bid_amount` DESC";
 
 		$result = $link_new->query($sql);
 		if ($result) {
@@ -59,12 +53,9 @@ class auctions
 		foreach ($attr as $key => $value) {
 			$attr[$key] = $link_new->real_escape_string($value);
 		}
-		$sql = ''
-			. 'SELECT '
-			. 'highest_bid, '
-			. 'highest_bidder '
-			. 'FROM game_data_auctions '
-			. "WHERE id = {$attr['auction_id']} ";
+		$sql = "SELECT `highest_bid`, `highest_bidder` 
+		FROM `game_data_auctions` 
+		WHERE `id` = {$attr['auction_id']}";
 
 		$result = $link_new->query($sql);
 		if ($result) {
@@ -90,7 +81,10 @@ class auctions
 			$attr[$key] = $link_new->real_escape_string($value);
 		}
 		$auction_data = false;
-		$result = $link_new->query("SELECT id, object_id, creator, status_code, minimum_price, instant_price, highest_bidder, highest_bid, end_date FROM `{$GLOBALS['DB_NAME_NEW']}`.game_data_auctions WHERE id = {$attr['auction_id']} AND status_code = 1 LIMIT 1");
+		$result = $link_new->query("SELECT `id`, `object_id`, `creator`, `status_code`, `minimum_price`, `instant_price`, `highest_bidder`, `highest_bid`, `end_date` 
+		FROM `{$GLOBALS['DB_NAME_NEW']}`.`game_data_auctions` 
+		WHERE `id` = {$attr['auction_id']} AND `status_code` = 1 
+		LIMIT 1");
 		if ($result) {
 			$auction_data = $result->fetch_assoc();
 		}
@@ -104,7 +98,9 @@ class auctions
 
 			if ($attr['mode'] == 'buy_now') {
 				$bid_date = new DateTime('now');
-				$sql = "SELECT stutteri, penge FROM `{$GLOBALS['DB_NAME_OLD']}`.Brugere WHERE id = {$_SESSION['user_id']} LIMIT 1";
+				$sql = "SELECT `stutteri`, `penge` 
+				FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` WHERE `id` = {$_SESSION['user_id']} 
+				LIMIT 1";
 				$result = $link_new->query($sql);
 				if ($result) {
 					$data = $result->fetch_assoc();
@@ -120,23 +116,31 @@ class auctions
 				}
 
 				if (is_null($auction_data['highest_bid'])) {
-					$link_new->query("UPDATE game_data_auctions SET highest_bidder = {$_SESSION['user_id']}, highest_bid = {$auction_data['instant_price']} WHERE  id = {$attr['auction_id']} AND status_code = 1 LIMIT 1");
+					$link_new->query("UPDATE `game_data_auctions` 
+					SET `highest_bidder` = {$_SESSION['user_id']}, `highest_bid` = {$auction_data['instant_price']} 
+					WHERE `id` = {$attr['auction_id']} AND `status_code` = 1 
+					LIMIT 1");
 				} else {
-					$link_new->query("UPDATE game_data_auctions SET highest_bidder = {$_SESSION['user_id']}, highest_bid = {$auction_data['instant_price']} WHERE highest_bidder = {$auction_data['highest_bidder']} AND highest_bid = {$auction_data['highest_bid']} AND id = {$attr['auction_id']} AND status_code = 1 LIMIT 1");
+					$link_new->query("UPDATE `game_data_auctions` 
+					SET `highest_bidder` = {$_SESSION['user_id']}, `highest_bid` = {$auction_data['instant_price']} 
+					WHERE `highest_bidder` = {$auction_data['highest_bidder']} AND `highest_bid` = {$auction_data['highest_bid']} AND `id` = {$attr['auction_id']} AND `status_code` = 1 
+					LIMIT 1");
 				}
 
 				if (mysqli_affected_rows($link_new) != 0) {
 					if (!is_null($auction_data['highest_bid'])) {
-						$link_new->query("UPDATE game_data_auction_bids SET status_code = 5 WHERE bid_amount = {$auction_data['highest_bid']} AND creator = {$auction_data['highest_bidder']} AND auction = {$auction_data['id']}");
-						$sql = "INSERT INTO game_data_private_messages "
-							. "(status_code, origin, target, date, message) "
-							. "VALUES "
-							. "(17, 52745, {$auction_data['highest_bidder']}, NOW(), 'Hesten med ID:{$auction_data['object_id']} er desværre blevet købt på auktionshuset, pengene {$auction_data['highest_bid']}wkr er returneret til din konto.')";
+						$link_new->query("UPDATE `game_data_auction_bids` 
+						SET `status_code` = 5 
+						WHERE `bid_amount` = {$auction_data['highest_bid']} AND `creator` = {$auction_data['highest_bidder']} AND `auction` = {$auction_data['id']}");
+						$sql = "INSERT INTO `game_data_private_messages` (`status_code`, `origin`, `target`, `date`, `message`) 
+						VALUES 
+						(17, 52745, {$auction_data['highest_bidder']}, NOW(), 'Hesten med ID:{$auction_data['object_id']} er desværre blevet købt på auktionshuset, pengene {$auction_data['highest_bid']}wkr er returneret til din konto.')";
 						$link_new->query($sql);
 						accounting::add_entry(['amount' => $auction_data['highest_bid'], 'line_text' => "Overbudt på auktion", 'mode' => '+', 'user_id' => $auction_data['highest_bidder']]);
 					}
 
-					$link_new->query("INSERT INTO game_data_auction_bids (creator, auction, bid_amount, bid_date, status_code) VALUES ({$_SESSION['user_id']}, {$attr['auction_id']}, {$auction_data['instant_price']}, '{$bid_date->format('Y-m-d H:i:s')}', 4)");
+					$link_new->query("INSERT INTO `game_data_auction_bids` (`creator`, `auction`, `bid_amount`, `bid_date`, `status_code`) 
+					VALUES ({$_SESSION['user_id']}, {$attr['auction_id']}, {$auction_data['instant_price']}, '{$bid_date->format('Y-m-d H:i:s')}', 4)");
 
 					$auction_fee = round(max(500, ($attr['instant_price'] * 0.005)), 0);
 					accounting::add_entry(['amount' => $auction_data['instant_price'], 'line_text' => "Din hest er solgt på auktion", 'mode' => '+', 'user_id' => $auction_data['creator']]);
@@ -150,7 +154,10 @@ class auctions
 
 			if ($attr['mode'] == 'place_bid') {
 				$bid_date = new DateTime('now');
-				$sql = "SELECT stutteri, penge FROM `{$GLOBALS['DB_NAME_OLD']}`.Brugere WHERE id = {$_SESSION['user_id']} LIMIT 1";
+				$sql = "SELECT `stutteri`, `penge` 
+				FROM `{$GLOBALS['DB_NAME_OLD']}`.`Brugere` 
+				WHERE `id` = {$_SESSION['user_id']} 
+				LIMIT 1";
 				$result = $link_new->query($sql);
 				if ($result) {
 					$data = $result->fetch_assoc();
@@ -171,22 +178,28 @@ class auctions
 				}
 
 				if (is_null($auction_data['highest_bid'])) {
-					$link_new->query("UPDATE game_data_auctions SET highest_bidder = {$_SESSION['user_id']}, highest_bid = {$attr['bid_amount']} WHERE  id = {$attr['auction_id']} AND status_code = 1 LIMIT 1");
+					$link_new->query("UPDATE `game_data_auctions` SET `highest_bidder` = {$_SESSION['user_id']}, `highest_bid` = {$attr['bid_amount']} 
+					WHERE `id` = {$attr['auction_id']} AND `status_code` = 1 
+					LIMIT 1");
 				} else {
-					$link_new->query("UPDATE game_data_auctions SET highest_bidder = {$_SESSION['user_id']}, highest_bid = {$attr['bid_amount']} WHERE highest_bidder = {$auction_data['highest_bidder']} AND highest_bid = {$auction_data['highest_bid']} AND id = {$attr['auction_id']} AND status_code = 1 LIMIT 1");
+					$link_new->query("UPDATE `game_data_auctions` SET `highest_bidder` = {$_SESSION['user_id']}, `highest_bid` = {$attr['bid_amount']} 
+					WHERE `highest_bidder` = {$auction_data['highest_bidder']} AND `highest_bid` = {$auction_data['highest_bid']} AND `id` = {$attr['auction_id']} AND `status_code` = 1 
+					LIMIT 1");
 				}
 
 				if (mysqli_affected_rows($link_new) != 0) {
 					if (!is_null($auction_data['highest_bid'])) {
-						$link_new->query("UPDATE game_data_auction_bids SET status_code = 5 WHERE bid_amount = {$auction_data['highest_bid']} AND creator = {$auction_data['highest_bidder']} AND auction = {$auction_data['id']}");
-						$sql = "INSERT INTO game_data_private_messages "
-							. "(status_code, origin, target, date, message) "
-							. "VALUES "
-							. "(17, 52745, {$auction_data['highest_bidder']}, NOW(), 'Du er desvære blevet overbudt på en auktion på hesten med ID:{$auction_data['object_id']} , pengene {$auction_data['highest_bid']}wkr er returneret til din konto.')";
+						$link_new->query("UPDATE `game_data_auction_bids` SET `status_code` = 5 
+						WHERE `bid_amount` = {$auction_data['highest_bid']} AND `creator` = {$auction_data['highest_bidder']} AND `auction` = {$auction_data['id']}");
+						$sql = "INSERT INTO `game_data_private_messages` 
+						(`status_code`, `origin`, `target`, `date`, `message`) 
+						VALUES 
+						(17, 52745, {$auction_data['highest_bidder']}, NOW(), 'Du er desvære blevet overbudt på en auktion på hesten med ID:{$auction_data['object_id']} , pengene {$auction_data['highest_bid']}wkr er returneret til din konto.')";
 						$link_new->query($sql);
 						accounting::add_entry(['amount' => $auction_data['highest_bid'], 'line_text' => "Overbudt på auktion", 'mode' => '+', 'user_id' => $auction_data['highest_bidder']]);
 					}
-					$link_new->query("INSERT INTO game_data_auction_bids (creator, auction, bid_amount, bid_date, status_code) VALUES ({$_SESSION['user_id']}, {$attr['auction_id']}, {$attr['bid_amount']}, '{$bid_date->format('Y-m-d H:i:s')}', 4)");
+					$link_new->query("INSERT INTO `game_data_auction_bids` (`creator`, `auction`, `bid_amount`, `bid_date`, `status_code`) 
+					VALUES ({$_SESSION['user_id']}, {$attr['auction_id']}, {$attr['bid_amount']}, '{$bid_date->format('Y-m-d H:i:s')}', 4)");
 					accounting::add_entry(['amount' => $attr['bid_amount'], 'line_text' => "Bud på auktion"]);
 					return ["Dit bud er registreret.", 'success'];
 				}
@@ -218,13 +231,11 @@ class auctions
 		//			game_data_auctions - id, creator, status_code, object_id, object_type, minimum_price, instant_price, creation_date, end_date
 		//			game_data_auction_bids - creator, auction, bid_amount, bid_date, status_code
 		//			game_data_status_codes - id, name, description = auction_live, auction_ended, auction_halted, bid_accepted, bid_refunded, bid_won
-		$sql = ''
-			. 'SELECT '
-			. '* '
-			. 'FROM game_data_auctions '
-			. 'WHERE status_code = 1 '
-			. (isset($attr['creator']) ? "AND creator = {$attr['creator']} " : '')
-			. 'ORDER BY end_date ASC, id ASC '
+		$sql = "SELECT * 
+		FROM `game_data_auctions` 
+		WHERE `status_code` = 1 "
+			. (isset($attr['creator']) ? "AND `creator` = {$attr['creator']} " : '')
+			. 'ORDER BY `end_date` ASC, `id` ASC '
 			. (isset($attr['limit']) ? "LIMIT {$attr['limit']} " : '')
 			. (isset($attr['offset']) ? "OFFSET {$attr['offset']} " : '');
 
@@ -250,13 +261,11 @@ class auctions
 		foreach ($attr as $key => $value) {
 			$attr[$key] = $link_new->real_escape_string($value);
 		}
-		$sql = ''
-			. 'SELECT '
-			. '* '
-			. 'FROM game_data_auctions '
-			. 'WHERE (status_code = 1 or status_code = 2) '
-			. "AND id = {$attr['id']} "
-			. "LIMIT 1";
+		$sql = "SELECT * 
+		FROM `game_data_auctions` 
+		WHERE (`status_code` = 1 or `status_code` = 2) 
+		AND `id` = {$attr['id']} 
+		LIMIT 1";
 
 		$result = $link_new->query($sql);
 
@@ -296,11 +305,11 @@ class auctions
 			foreach ($horses as $horse) {
 				if ($horse['id'] == ($attr['horse_id'])) {
 					/* Transfer horse to auctions user */
-					$sql = "UPDATE `{$GLOBALS['DB_NAME_OLD']}`.Heste SET bruger = 'Auktionshuset' WHERE id = '{$horse['id']}' AND bruger = '{$seller_user_name}'";
+					$sql = "UPDATE `{$GLOBALS['DB_NAME_OLD']}`.`Heste` SET `bruger` = 'Auktionshuset' WHERE `id` = '{$horse['id']}' AND `bruger` = '{$seller_user_name}'";
 					$result = $link_new->query($sql);
 					/* Insert horse into auctions table */
-					$sql = "INSERT INTO game_data_auctions (creator, status_code, object_id, object_type, minimum_price, instant_price, creation_date, end_date) "
-						. "VALUES ('{$attr['seller_id']}', '1', '{$attr['horse_id']}', '1', '{$attr['minimum_bid']}','{$attr['buy_now_price']}', NOW(), '{$attr['sell_date']} 17:00:00' )";
+					$sql = "INSERT INTO `game_data_auctions` (`creator`, `status_code`, `object_id`, `object_type`, `minimum_price`, `instant_price`, `creation_date`, `end_date`) 
+					VALUES ('{$attr['seller_id']}', '1', '{$attr['horse_id']}', '1', '{$attr['minimum_bid']}','{$attr['buy_now_price']}', NOW(), '{$attr['sell_date']} 17:00:00' )";
 					$result = $link_new->query($sql);
 					/* TODO */
 					/* Verify that horse is in auctions table or transfer back to user */
