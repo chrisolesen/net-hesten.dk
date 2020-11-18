d31799e5b5781163c4c1b00054423fcf9badb014
Chris Olesen <privat@chrisolesen.net>
2020-11-18 09:04:23 +0100

    fixed timezones

--- app_core/enviroment_equalizer.php
+++ app_core/enviroment_equalizer.php
@@ -2,11 +2,12 @@
 
 if (defined('PROJECT_TIMEZONE')) {
     date_default_timezone_set(PROJECT_TIMEZONE);
-    $time_zone_offset = ((new DateTime('now'))->getOffset() / 60 / 60) . ':00';
-    if ($time_zone_offset == '0:00') {
-        $time_zone_offset = '-' . $time_zone_offset;
+    $time_zone_offset = ((new DateTime('now'))->getOffset() / 60 / 60);
+    if ($time_zone_offset >= 0) {
+        $time_zone_offset = '+' . $time_zone_offset;
     }
-    $result = $link_new->query("SET time_zone = '{$time_zone_offset}'");
-    $GLOBALS['pdo_new']->exec("SET time_zone = '{$time_zone_offset}'");
-    $GLOBALS['pdo_old']->exec("SET time_zone = '{$time_zone_offset}'");
+
+    $link_new->query("SET time_zone = '{$time_zone_offset}:00'");
+    $GLOBALS['pdo_new']->exec("SET time_zone = '{$time_zone_offset}:00'");
+    $GLOBALS['pdo_old']->exec("SET time_zone = '{$time_zone_offset}:00'");
 }
