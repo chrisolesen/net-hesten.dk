b503e50dca2e550368da92c027ff8e187e0d13b8
Chris Olesen <privat@chrisolesen.net>
2020-11-18 08:44:15 +0100

    Update enviroment_equalizer.php

--- app_core/enviroment_equalizer.php
+++ app_core/enviroment_equalizer.php
@@ -3,6 +3,9 @@
 if (defined('PROJECT_TIMEZONE')) {
     date_default_timezone_set(PROJECT_TIMEZONE);
     $time_zone_offset = ((new DateTime('now'))->getOffset() / 60 / 60) . ':00';
+    if ($time_zone_offset == '0:00') {
+        $time_zone_offset = '-' . $time_zone_offset;
+    }
     $result = $link_new->query("SET time_zone = '{$time_zone_offset}'");
     $GLOBALS['pdo_new']->exec("SET time_zone = '{$time_zone_offset}'");
     $GLOBALS['pdo_old']->exec("SET time_zone = '{$time_zone_offset}'");
