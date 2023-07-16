6f7e899a087d5666a3cce3cdf2ada3cb56f9c0b3
Chris Olesen <clo@embrace-it.com>
2023-07-16 11:47:11 +0200

    included caches in installer

--- install.net-hesten.dk/index.php
+++ install.net-hesten.dk/index.php
@@ -6,7 +6,9 @@
 </head>
 
 <body>
-    <a href="/setup_tables.php">Setup tables</a> 
+    <a href="/setup_tables.php">Setup tables</a>
     <a href="/basic_data.php">Enter basic data</a>
+    <a href="/build_caches.php">Build default caches</a>
 </body>
+
 </html>
