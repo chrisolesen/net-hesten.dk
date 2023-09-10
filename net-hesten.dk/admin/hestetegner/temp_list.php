a34942186f0f7349b9803de1cca2cd546097933d
Chris Olesen <clo@embrace-it.com>
2023-09-10 21:39:39 +0200

    Update temp_list.php

--- net-hesten.dk/admin/hestetegner/temp_list.php
+++ net-hesten.dk/admin/hestetegner/temp_list.php
@@ -248,7 +248,7 @@ $selected_race = substr($_GET['race'], 1, -1);
 
 
 		
-		$sql = "SELECT `image`, `status`, `id`, `allowed_gender`, `archived` FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` ORDER BY `date` DESC";
+		$sql = "SELECT `image`, `status`, `id`, `allowed_gender`, `archived` FROM `{$GLOBALS['DB_NAME_NEW']}`.`horse_types` LIMIT 20 ORDER BY `date` DESC";
 		$result = $link_new->query($sql);
 		$races = '';
 		$latin_dead = 'død';
